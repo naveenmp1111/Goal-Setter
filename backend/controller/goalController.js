54039c19fd2f77336db07e68316c4ff65772c9e1
Naveen <naveenapk048@gmail.com>
2024-03-11 11:40:30 +0530

    Rest api for goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,22 +1,56 @@
+const Goal=require('../model/goalModel')
 const asyncHandler=require('express-async-handler')
 
+
 const getGoal = asyncHandler(async(req, res) => {
-    res.json({ message: 'get Goals' })
+    const goal=await Goal.find()
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    res.status(200).json(goal)
 })
+
 const setGoal =  asyncHandler(async(req, res) => {
     console.log(req.body)
     if (!req.body.text) {
         res.status(400)
         throw new Error('add some data')
     }
-    res.status(200).json({ message: 'set Goals' })
+        
+    const goal=await Goal.create({
+        text:req.body.text
+    })
+
+    res.status(200).json(goal)
 })
+
 const updateGoal =  asyncHandler(async(req, res) => {
     const data = req.params.id
-    res.status(200).json({ message: `update Goals ${data}` })
+    const goal=await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{new:true})
+
+    res.status(200).json(updatedGoal)
 })
+
 const deleteGoal =  asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `delete Goals ${req.params.id}` })
+    
+    const goal=Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await Goal.findByIdAndDelete(req.params.id)
+    res.status(200).json({id:req.params.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express=require('express')
+const colors=require('colors')
 const dotenv=require('dotenv').config()
 const port=process.env.PORT || 5000
 const router=require('./routes/goalRouter')
 const {errorHandler} =require('./middleware/errorHandler')
+const connectDB=require('./config/db')
+
+connectDB()
 
 const app=express()
 
